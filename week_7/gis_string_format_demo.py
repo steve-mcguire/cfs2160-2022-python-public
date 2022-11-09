cff603631313ebafa6a672a8d0147b94b56455c9
Steve McGuire <s.mcguire@hud.ac.uk>
2022-11-09 12:01:38 +0000

    GIS week 7 material

---
+++ week_7/gis_string_format_demo.py
@@ -0,0 +1,9 @@
+# Tutorialspoint. (nd). Python Strings. https://www.tutorialspoint.com/python3/python_strings.htm
+
+firstname = "Steve"
+lastname = "McGuire"
+age = 44
+
+formatted_string = "Firstname: %s, Lastname: %s, age: %d" % (firstname, lastname, age)
+
+print(formatted_string)
---
+++ week_7/gis_string_slice_demo.py
@@ -0,0 +1,7 @@
+# Tutorialspoint. (nd). Python Strings. https://www.tutorialspoint.com/python3/python_strings.htm
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print("var1[0]: ", var1[0])
+print("var2[1:5]: ", var2[1:5])
